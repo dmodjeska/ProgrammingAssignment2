2c1e13b1cc1055cd8da4a82a21f0378c180246ca
David Modjeska <modjeska@acm.org>
2015-03-20 12:11:09 -0400

    Drafted functions from templates
    Adapted Coursera sample code for caching the mean of a vector, so that
    it would work for caching the solution of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions support creating, modifying and solving cached matrices
+## The goal is to generate performance improvements for large data sets
 
-## Write a short comment describing this function
+
+## Return a cached matrix with accessors and mutators
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## initalize solution
+        m <- NULL
+        
+        ## create accessors and mutators
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        
+        ## return cached matrix
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of argument 'x'
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {              
+        ## if solution is cached, print message and return cached solution
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        ## if solution is not cached, solve matrix, cache new solution, and return this solution
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
